171b61d99e1a0c3df05ccfe589f9f43d9f648951
Naftoli Gugenheim <naftoligug@gmail.com>
2012-09-30 15:41:26 -0400

    Get rid of everything that has nothing to do with the issue

--- src/main/scala/code/snippet/Countries.scala
+++ src/main/scala/code/snippet/Countries.scala
@@ -2,26 +2,18 @@ package code
 package snippet
 
 import model._
-import lib.util._
 import net.liftweb._
 import common._
 import util._
 import Helpers._
-import sitemap._
 
-object Countries extends Loggable {
-  logger.info("snippet.Countries obj start")
-  
-  val ccList:List[Country]  = Country.getAllList()
-  
-  lazy val menu = Menu.i("Countries") / "countries"
+object Countries {
+  val ccList:List[Country]  = Nil
 
-  def render = { 
-    "a"        #> ccList.map( c => 
-    "* [href]" #> ACountry.menu.calcHref(c) & 
-    "* *"      #> (c.name) //in 2.4 the code worked without a added .is call, fails in 2.5-M1 without the added .get or .is call.
+  def render = {
+    "a"        #> ccList.map( c =>
+      "* *"      #> (c.name) //in 2.4 the code worked without a added .is call, fails in 2.5-M1 without the added .get or .is call.
         )
   }
-  
-  logger.info("snippet.Countries obj stop")
+
 }
